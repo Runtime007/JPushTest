be83a215e43148f5411c36c53a1f7b8b758dcf49
leibin <bin.leibin.bat@gmail.com>
2015-11-12 14:59:27 +0800

    change MainActivity

--- app/src/main/java/bat/bin/com/myapplication3/MainActivity.java
+++ app/src/main/java/bat/bin/com/myapplication3/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends InstrumentedActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //why did you do that!
 
     }
 
